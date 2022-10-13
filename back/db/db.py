d90957b54a8a1c81976cb0c13496bd4064662ca3
drawholic <mamrokhavlad@gmail.com>
2022-10-13 18:20:29 +0300

    reqs.txt upd

--- back/db/db.py
+++ back/db/db.py
@@ -11,7 +11,7 @@ DB_PASS=os.getenv('PG_PASS')
 DB_DB=os.getenv('PG_DB')
 
 
-DB_URL = f`postgresql://${DB_USER}:${DB_PASS}@db:5432/${DB_DB}`
+DB_URL = f'postgresql://{DB_USER}:{DB_PASS}@db:5432/{DB_DB}'
    
 
 database = databases.Database(DB_URL)
