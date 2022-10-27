90921f110229be73474fc3e3f79938a5f47856c6
drawholic <mamrokhavlad@gmail.com>
2022-10-27 15:37:10 +0300

    clear some files, added new ignores to .gitignore for .idea

--- back/app.py
+++ back/app.py
@@ -35,18 +35,17 @@ async def startup():
     await init_models()
 
     logger.info('SERVER STARTED')
-    await database.connect()
     redis = await aioredis.from_url('redis://localhost')
     # some actions with redis in future    
     
     await redis.close()
+
 
 @app.on_event('shutdown')
 async def shutdown():
     logger.info('server is stopping, connection to db is closing...')
     await database.disconnect()
     
-
 
 @app.get('/')
 def index():
